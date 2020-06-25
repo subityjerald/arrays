30233b0f63d953e67c2e35c2965afe1eb02cfa11
subity jerald <subityjerald1994@gmail.com>
2020-06-25 17:03:08 +0530

    arra

---
+++ src/Integer.java
@@ -0,0 +1,23 @@
+
+public class Integer {
+public static void main(String args[]) {
+String a="python";
+	
+	System.out.println(a.substring(3)+a+a.substring(3));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*ava program to take the last three characters from a given string and add the three characters 
+ * at both the front and back of the string. String length must be greater than three and more.
+ */
